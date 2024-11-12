93c1710de378961127db370f58f587a4fdb40f76
AvinashS97 <avikumar97170@gmail.com>
2024-11-12 17:55:25 +0530

    Collection

--- src/q_Collection_Framework/Fruit.java
+++ src/q_Collection_Framework/Fruit.java
@@ -1,7 +1,13 @@
 package q_Collection_Framework;
 
 public class Fruit {
-	public static void eat() {
-		System.out.println("Eat Method Started");
+	public Fruit fruit() {
+		return new Fruit();
+	}
+	public void eat() {
+		System.out.println("Eat method");
+	}
+	public void makeJuice() {
+		System.out.println("Make Juice");
 	}
 }
--- src/q_Collection_Framework/FruitDriver.java
+++ src/q_Collection_Framework/FruitDriver.java
@@ -3,6 +3,8 @@ package q_Collection_Framework;
 public class FruitDriver {
 	public static void main(String[] args) {
 		Fruit f1 = new Fruit();
-		f1.eat();
+		Fruit x = f1.fruit();
+		x.eat();
+		x.makeJuice();
 	}
 }
